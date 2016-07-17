962919288eb75b72c26164ca5c7553ed66da200f
Denis Benato <benato.denis96@gmail.com>
2016-07-17 18:15:34 +0200

    Style fixes

--- Gishiki/Pipeline/PipelineCollector.php
+++ Gishiki/Pipeline/PipelineCollector.php
@@ -17,33 +17,74 @@ limitations under the License.
 
 namespace Gishiki\Pipeline;
 
-use Gishiki\Pipeline\Pipeline;
-use Gishiki\Pipeline\PipelineException;
-
 /**
  * Represent the entire collection of pipelines currectly available.
  * 
  * @author Benato Denis <benato.denis96@gmail.com>
  */
-abstract class PipelineCollector {
+abstract class PipelineCollector
+{
     private static $pipelines = array();
-    
+
     /**
-     * Register a pipeline in the global pipeline collector
+     * Register a pipeline in the global pipeline collector.
      * 
-     * @param  Pipeline $pipeline the pipeline to be registered
-     * @throws PipelineException  a pipeline with the same name already eexists
+     * @param Pipeline $pipeline the pipeline to be registered
+     *
+     * @throws PipelineException a pipeline with the same name already eexists
      */
     public static function registerPipeline(Pipeline &$pipeline)
     {
+        //register the current pipeline only if a pipeline with the same name doesn't exists
+        if (self::checkPipelineByName($pipeline->getName())) {
+            throw new PipelineException('A pipeline with the same name already exists', 0);
+        }
+        self::$pipelines[] = &$pipeline;
+    }
+
+    /**
+     * Check if a pipeline with the given name already exists.
+     * 
+     * @param string $name the name of the pipeline to be searched
+     *
+     * @return bool TRUE only is a pipeline with the given name already exists
+     */
+    public static function checkPipelineByName($name)
+    {
+        try {
+            self::getPipelineByName($name);
+
+            return true;
+        } catch (PipelineException $ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Get a pipeline using its name.
+     * 
+     * @param string $name the name of the desired pipeline
+     *
+     * @return Pipeline& the reference to the pipeline with the given name
+     *
+     * @throws \InvalidArgumentException the given pipeline name is not valid
+     * @throws PipelineException         a pipeline with the given name cannot be found
+     */
+    public static function getPipelineByName($name)
+    {
+        //check for the name
+        if ((!is_string($name)) || (strlen($name) <= 0)) {
+            throw new \InvalidArgumentException('the given pipeline name is not a valid string');
+        }
+
         //check for another pipeline with the same name
-        foreach (self::$pipelines as $pipelineToCheck) {
-            if ($pipelineToCheck->getName() == $pipeline->getName()) {
-                throw new PipelineException("A pipeline with the same name already exists", 0);
+        foreach (self::$pipelines as &$pipelineToCheck) {
+            if (strcmp($pipelineToCheck->getName(), $name) == 0) {
+                return $pipelineToCheck;
             }
         }
-        
-        //register the pipeline
-        self::$pipelines[] = &$pipeline;
+
+        //if executing this than the correct pipeline was not returned
+        throw new PipelineException("A pipeline with the given name doesn't exists", 1);
     }
 }

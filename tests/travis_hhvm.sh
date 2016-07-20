f4fcab0fea41070aa8f815f77242c529709d65ce
Denis Benato <benato.denis96@gmail.com>
2016-07-20 14:30:43 +0200

    Travis-CI for HHVM and PHP

---
+++ tests/travis_hhvm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/mongodb/mongo-hhvm-driver.git
+cd mongo-hhvm-driver
+git submodule sync && git submodule update --init --recursive
+hphpize
+cmake .
+make configlib
+make -j 5
+sudo make install
+cd ../
---
+++ tests/travis_php.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pecl config-set php_ini .travis.php.ini
+pear config-set php_ini .travis.php.ini
+pecl install mongodb-1.1.8

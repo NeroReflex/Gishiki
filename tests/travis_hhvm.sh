64d4359c1826ca0032258b0dbd23be375a750c57
Denis Benato <benato.denis96@gmail.com>
2016-07-20 15:22:38 +0200

    Better HHVM

--- tests/travis_hhvm.sh
+++ tests/travis_hhvm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sudo apt-get install -y hhvm-dev
 git clone https://github.com/mongodb/mongo-hhvm-driver.git
 cd mongo-hhvm-driver
-git submodule sync && git submodule update --init --recursive
+git submodule sync
+git submodule update --init --recursive
 hphpize
 cmake .
 make configlib

513fb533af52058b292351bf46cfed6f75a44227
Denis Benato <benato.denis96@gmail.com>
2016-07-20 15:50:43 +0200

    Installing mongo extension

--- tests/travis_hhvm.sh
+++ tests/travis_hhvm.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-sudo apt-get install -y hhvm-dev
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo update-alternatives --remove-all gcc 
+sudo update-alternatives --remove-all g++ 
+sudo apt-get update
+sudo apt-get install g++-4.8 gcc-4.8 hhvm-dev -y
+sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
 git clone https://github.com/mongodb/mongo-hhvm-driver.git
 cd mongo-hhvm-driver
 git submodule sync

e60c1c6ce4f2982604aa6a0d6dad4f003624b138
Denis Benato <benato.denis96@gmail.com>
2016-08-09 12:47:35 +0200

    Added preloaded routes

---
+++ Gishiki/frameworkSetup.php
@@ -0,0 +1,48 @@
+<?php
+/**************************************************************************
+Copyright 2016 Benato Denis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*****************************************************************************/
+
+use Gishiki\Gishiki;
+use Gishiki\Core\Route;
+use Gishiki\Logging\Logger;
+use Gishiki\HttpKernel\Request;
+use Gishiki\HttpKernel\Response;
+use Gishiki\Algorithms\Collections\SerializableCollection;
+
+Route::get('/info', function (Request $request, Response &$response, SerializableCollection &$arguments)
+{
+    $info = new SerializableCollection(
+            [
+                'Framework' => 'Gishiki',
+                'PHP' => phpversion(),
+            ]);
+    
+    //write the response
+    $response->setSerializedBody($info);
+});
+
+Route::get('/serverkey', function (Request $request, Response &$response, SerializableCollection &$arguments)
+{
+    //get the serialized public key
+    $serializedPubKey = (new \Gishiki\Security\Encryption\Asymmetric\PrivateKey())->exportPublicKey();
+    
+    //write the response
+    $response->withStatus(200);
+    $response->write($serializedPubKey);
+});
+
+//run an instance of the application
+Gishiki::Run();

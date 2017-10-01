58e2ff4827da33d3b446720ff3318d268f68922a
Denis Benato <benato.denis96@gmail.com>
2017-10-02 00:49:00 +0200

    fixed a test

--- tests/Algorithms/Strings/SimpleLexerTest.php
+++ tests/Algorithms/Strings/SimpleLexerTest.php
@@ -71,9 +71,9 @@ class SimpleLexerTest extends TestCase
         $this->assertEquals(false, SimpleLexer::isUnsignedInteger("0.0"));
         $this->assertEquals(false, SimpleLexer::isUnsignedInteger("0a5"));
         $this->assertEquals(false, SimpleLexer::isUnsignedInteger(5));
+        $this->assertEquals(false, SimpleLexer::isUnsignedInteger("+1"));
 
         $this->assertEquals(true, SimpleLexer::isUnsignedInteger("8"));
-        $this->assertEquals(true, SimpleLexer::isUnsignedInteger("+1"));
         $this->assertEquals(true, SimpleLexer::isUnsignedInteger("-2"));
         $this->assertEquals(true, SimpleLexer::isUnsignedInteger("-57"));
         $this->assertEquals(true, SimpleLexer::isUnsignedInteger("+50"));
